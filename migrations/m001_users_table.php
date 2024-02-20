3b38cfa5487b42f5729ca939ac224baa86286176
Saqlainkhadim <Saqlainkhadimbusiness@gmail.com>
2024-02-20 22:06:37 +0500

    created migrations to add table

--- migrations/m001_users_table.php
+++ migrations/m001_users_table.php
@@ -6,11 +6,17 @@ class m001_users_table implements Migration
 {
     public function up(): void
     {
-        echo "migration code" . PHP_EOL;
+        \app\core\Application::$app->db->pdo->exec(" CREATE TABLE users(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            email VARCHAR(225) NOT NULL,
+            first_name VARCHAR(225) NOT NULL,
+            last_name VARCHAR(225) NOT NULL,
+            status TINYINT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ) engine=INNODB;");
     }
 
     public function down(): void
     {
-        echo "migration down code" . PHP_EOL;
+        \app\core\Application::$app->db->pdo->exec("Drop TABLE users If EXISTS;");
     }
 }
--- migrations/m002_add_password_column_to_users_table.php
+++ migrations/m002_add_password_column_to_users_table.php
@@ -6,11 +6,11 @@ class m002_add_password_column_to_users_table implements Migration
 {
     public function up(): void
     {
-        echo "migration code" . PHP_EOL;
+        \app\core\Application::$app->db->pdo->exec("Alter TABLE users add column password varchar(225) Not null;");
     }
 
     public function down(): void
     {
-        echo "migration down code" . PHP_EOL;
+        \app\core\Application::$app->db->pdo->exec("Alter TABLE users Drop column password;");
     }
 }
